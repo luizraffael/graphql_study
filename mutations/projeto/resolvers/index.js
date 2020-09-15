cbd991cbdf3c3abc1462ec7eda646702b0581b33
Luiz Rafael <luiz.mascarenhas@sambatech.com.br>
2020-09-15 12:05:40 -0300

    CRUD
    Crud

---
+++ mutations/projeto/resolvers/index.js
@@ -0,0 +1,10 @@
+const Query = require('./Query')
+const Usuario = require('./Usuario')
+const Mutation = require('./Mutation')
+
+module.exports = {
+    Query,
+    Mutation,
+    Usuario
+    
+}
