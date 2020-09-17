3660d911e316993e56b65ca8841bef6d9f6ccda3
Luiz Rafael <luiz.mascarenhas@sambatech.com.br>
2020-09-16 21:39:28 -0300

    add inputs final
    add inputs final

--- mutations/projeto/resolvers/Query.js
+++ mutations/projeto/resolvers/Query.js
@@ -1,13 +1,28 @@
 const { usuarios, perfis } = require('../data/db')
 
+function indiceUsuario(filter){
+    if (!filter) return -1
+    const {id, email} = filter 
+    if (id){
+        return usuarios.findIndex(u => u.id === id)
+    }else if (email){
+        return usuarios.findIndex(u => u.email === email)
+    }
+    return -1
+}
+
 module.exports = {
     usuarios() {
         return usuarios
     },
-    usuario(_, { id }) {
-        const sels = usuarios
-            .filter(u => u.id === id)
-        return sels ? sels[0] : null
+    usuario(_, { filter}) {
+       // const sels = usuarios
+        //    .filter(u => u.id === id)
+        //return sels ? sels[0] : null
+        const i = indiceUsuario(filter)
+        if (i < 0 ) return null
+        return usuarios [1]
+
     },
     perfis() {
         return perfis
