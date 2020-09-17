4ffed983b2cddcb781e915611c8a43c00ffdc78f
Luiz Rafael <luiz.mascarenhas@sambatech.com.br>
2020-09-16 21:39:02 -0300

    add inputs
    add inputs

--- mutations/projeto/resolvers/Mutation.js
+++ mutations/projeto/resolvers/Mutation.js
@@ -1,23 +1,34 @@
 const {usuarios, proximoId} = require ('../data/db')
 
+function indiceUsuario(filter){
+    if (!filter) return -1
+    const {id, email} = filter 
+    if (id){
+        return usuarios.findIndex(u => u.id === id)
+    }else if (email){
+        return usuarios.findIndex(u => u.email === email)
+    }
+    return -1
+}
+
 module.exports = {
     //nome, email, idade
-    novoUsuario(_, args){
-        const emailExistente = usuarios.some(u => u.email === args.email)
+    novoUsuario(_, {dados }){
+        const emailExistente = usuarios.some(u => u.email === dados.email)
         if(emailExistente){
             throw new Error('E-mail cadastrado')
         }
         const novo = {
             id: proximoId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO'
         }
         usuarios.push(novo)
         return novo
     },
-    excluirUsuario(_, {id}){
-        const i = usuarios.findIndex(u => u.id === id)
+    excluirUsuario(_, {filter}){
+        const i = indiceUsuario(filter)
         if (i < 0) return null
         const excluidos = usuarios.splice(i ,1)
         return excluidos ? excluidos[0] : null
